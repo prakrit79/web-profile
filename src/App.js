e47fb1c5659dbf56b5230880a1cf42bc78897cbb
Prakrit Baidya <baidya.prakrit@gmail.com>
2019-02-01 14:38:01 -0800

    Added React-mdl menu and added new component pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Layout, Navigation, Header,Drawer,Content } from 'react-mdl';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1> Prakrits Portfolio </h1>
+      <div style={{height: '300px', position: 'relative'}}>
+        <Layout style={{background: 'url(https://dl.dropbox.com/s/89ufwo88kov1xrp/SAM_1853.jpg) center / cover'}}>
+          <Header transparent title="Prakrit's Portfolio" style={{color: 'Black'}}>
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+          </Header>
+          <Drawer title="Menu">
+            <Navigation>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+                <a href="/">Link</a>
+            </Navigation>
+        </Drawer>
+        <Content />
+        </Layout>
       </div>
     );
   }
