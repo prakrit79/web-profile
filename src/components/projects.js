b8a8207d1d767f0ae0df8d07b94f53ac22348f68
Prakrit Baidya <baidya.prakrit@gmail.com>
2019-02-08 14:59:48 -0800

    Fixed UBC logo margins, added project info for each project

--- src/components/projects.js
+++ src/components/projects.js
@@ -9,13 +9,16 @@ class Projects extends Component{
 				<Grid className = "project-grid">
 					<div className = "project-links">
 						<Cell col = {4}>
-							<Card shadow={0} style={{width: '200px', height: '100%', margin: 'auto'}}>
-    							<CardTitle expand style={{background: 'url(https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-128.png) center 20% no-repeat'}}>
-    								Web Portfolio
+							<Card shadow={0} style={{width: '220px',height: '457px', margin: 'auto'}}>
+    							<CardTitle style={{height: '176px', background: 'url(https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-128.png) center / contain no-repeat'}}>
     							</CardTitle>
+                                    Web Portfolio
    								 <CardText>
         							CSS | HTML | React
     							</CardText>
+                                <CardText>
+                                    I wanted to create a web profile that showcases my passion for web development and technology. I used the simiplicity of React components along with CSS to create a simple online porfolio.
+                                </CardText>
     							<CardActions border>
         							<Button onClick={() => window.open('https://github.com/prakrit79/web-profile')}colored>Github</Button>
         							<Button onClick={() => window.open('https://web-portfolio-prakrit.herokuapp.com/')}colored>Live</Button>
@@ -23,26 +26,32 @@ class Projects extends Component{
 							</Card>
 						</Cell>
 						<Cell col = {4}>
-							<Card shadow={0} style={{width: '200px', height: '100%', margin: 'auto'}}>
-    							<CardTitle expand style={{background: 'url(https://cdn2.iconfinder.com/data/icons/nodejs-1/128/nodejs-128.png) center 20%  no-repeat'}}>
-    								Concerts Map
+							<Card shadow={0} style={{width: '220px', height: '457px', margin: 'auto'}}>
+    							<CardTitle style={{height: '176px', background: 'url(https://cdn2.iconfinder.com/data/icons/nodejs-1/128/nodejs-128.png) center / contain no-repeat'}}>
     							</CardTitle>
+                                    Concerts Map
    								 <CardText>
         							JavaScript | JSON | REST
     							</CardText>
+                                 <CardText>
+                                    There are many artists people want to see live. We wanted to create a service that made it easy to visualize where an artist frequently travels so you can be prepared for your next show.
+                                </CardText>
     							<CardActions border>
         							<Button onClick={() => window.open('https://github.com/prakrit79/ConcertsMap')} colored>Github</Button>
     							</CardActions>
 							</Card>
 						</Cell>
 						<Cell col = {4}>
-							<Card shadow={0} style={{width: '200px', height: '100%', margin: 'auto'}}>
-    							<CardTitle expand style={{background: 'url(https://liuji-jim.gallerycdn.vsassets.io/extensions/liuji-jim/vue/0.1.5/1478501659069/Microsoft.VisualStudio.Services.Icons.Default) center 20% no-repeat'}}>
-    								 Clinic Data
+							<Card shadow={0} style={{width: '220px',height: '457px', margin: 'auto'}}>
+    							<CardTitle style={{height: '176px', background: 'url(https://liuji-jim.gallerycdn.vsassets.io/extensions/liuji-jim/vue/0.1.5/1478501659069/Microsoft.VisualStudio.Services.Icons.Default) center / contain no-repeat'}}>
     							</CardTitle>
+                                    Clinic Database
    								 <CardText>
         							CSS | Vue JS | SQL
     							</CardText>
+                                <CardText>
+                                    Me and my team created a clinic database using SQL and Vue that would allow a medical clinic to operate efficiently through the help of direct appointment, referral as well as prescription system.
+                                </CardText>
     							<CardActions border>
         							<Button onClick={() => window.open('https://github.com/prakrit79/cpsc304-project-starter')}colored>Github</Button>
     							</CardActions>
